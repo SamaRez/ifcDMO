b28c3cfb03c19fa792d2bcbdae26819c48a3039f
SamaRez <rezvani.sama@gmail.com>
2024-01-04 17:05:05 +0100

    added the header details

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ viewer = st.container()
 report = st.container()
 graphs = st.container()
 #--------------------------
+#HEADER
+#Page Header
+with header:
+    st.title("Speckle Stream Activity App📈")
+#About info
+with header.expander("About this app🔽", expanded=True):
+    st.markdown(
+        """This is a beginner web app developed using Streamlit. My goal was to understand how to interact with Speckle API using SpecklePy, 
+        analyze what is received and its structure. This was easy and fun experiment.
+        """
+    )
+#--------------------------
